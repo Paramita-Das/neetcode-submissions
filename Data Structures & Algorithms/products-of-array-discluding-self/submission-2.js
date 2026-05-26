d84d32315c845690f235433c5fbe8362d3edc27c
Paramita-Das <84445917+Paramita-Das@users.noreply.github.com>
2026-05-26 16:13:58 +0530

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let prefix = Array(nums.length).fill(1);
+        let suffix = Array(nums.length).fill(1);
+        for (let i = 1; i < nums.length; i++) {
+            prefix[i] = prefix[i - 1] * nums[i - 1]
+        }
+          for (let i = nums.length - 2; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] * nums[i + 1]
+        }
+        for (let i = 0; i < nums.length; i++) {
+            nums[i] = prefix[i] * suffix[i]
+        }
+        return nums;
+    }
+}
