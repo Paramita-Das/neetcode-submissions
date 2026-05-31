9687623685bf1282786d9c7df5f538c3662d70f8
Paramita-Das <84445917+Paramita-Das@users.noreply.github.com>
2026-05-31 18:27:17 +0530

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height) {
+        let n = height.length;
+        let left = 0;
+        let right = n - 1;
+        let leftMax = 0;
+        let rightMax = 0;
+        let water = 0;
+       while (left < right) {
+        leftMax = Math.max(leftMax, height[left]);
+        rightMax = Math.max(rightMax, height[right]);
+       if (leftMax < rightMax) {
+        water += leftMax - height[left];
+        left++
+       } else {
+        water += rightMax - height[right];
+        right--
+       }
+       }
+        return water
+    }
+}
