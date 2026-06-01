66056a1b4cfa386f12bee402bb692e2c3e545365
Paramita-Das <84445917+Paramita-Das@users.noreply.github.com>
2026-06-02 01:04:06 +0530

    Add: longest-repeating-substring-with-replacement - submission-6

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-6.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        let map = new Map();
+        let maxLen = 0;
+        let maxFreq = 0;
+        let left = 0;
+        for (let right = 0; right < s.length; right++) {
+            map.set(s[right], (map.get(s[right]) || 0) + 1);
+            maxFreq = Math.max(maxFreq, map.get(s[right]));
+            while ((right - left + 1) - maxFreq > k) {
+                map.set(s[left], map.get(s[left]) - 1)
+                left++;
+            } 
+            maxLen = Math.max(maxLen, right - left + 1)
+        }
+        return maxLen;
+    }
+}
