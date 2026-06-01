8c4227d15d430e1360e357ead04c284e28fba9dc
Paramita-Das <84445917+Paramita-Das@users.noreply.github.com>
2026-06-01 17:17:01 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let maxProfit = 0;
+        let minPrice = prices[0];
+        for (let i = 0; i < prices.length; i++) {
+            minPrice = Math.min(minPrice, prices[i]);
+            let profit = prices[i] - minPrice;
+            maxProfit = Math.max(maxProfit, profit)
+        }
+        return maxProfit
+    }
+}
