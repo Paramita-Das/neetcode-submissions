3a0dc7e9e7576ee2c477d47b387b28cf2f731ae4
Paramita-Das <84445917+Paramita-Das@users.noreply.github.com>
2026-07-17 20:33:39 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+        let left = 1;
+        let right = Math.max(...piles);
+        let answer = 0;
+        function canFinish(k) {
+            let hours = 0;
+            for (let pile of piles) {
+                hours += Math.ceil(pile / k)
+            }
+           return hours > h ? false : true
+        }
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2);
+        if (canFinish(mid)) {
+           answer = mid; 
+            right = mid - 1
+        } else {
+            left = mid + 1
+        }
+        }
+        return answer
+    }
+}
