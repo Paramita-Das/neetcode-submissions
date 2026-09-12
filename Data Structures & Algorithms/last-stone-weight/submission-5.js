880bbf113566d14ea9b4443c0da1d5708c218103
Paramita-Das <84445917+Paramita-Das@users.noreply.github.com>
2026-09-12 20:18:50 +0530

    Add: last-stone-weight - submission-5

---
+++ Data Structures & Algorithms/last-stone-weight/submission-5.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} stones
+     * @return {number}
+     */
+    lastStoneWeight(stones) {
+        let heap = new MaxPriorityQueue((num) => num);
+        let newArr = [];
+        // if (stones.length < 2) {
+        //     return stones[0]
+        // }
+        for (let stone of stones) {
+            heap.enqueue(stone);
+        }
+        while (heap.size() > 1) {
+            let y = heap.dequeue();
+            let x = heap.dequeue();
+            if (x !== y) {
+                heap.enqueue(y - x)
+            }
+        }
+        return heap.front() || 0
+    }
+}
